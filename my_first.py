dcd67f1c5d7b023c485cf6662daed3cd4da21617
Leena Hunagund <109791804+leenahunagund@users.noreply.github.com>
2023-08-23 16:45:09 +0530

    my_first.py

--- my_first.py
+++ my_first.py
@@ -10,6 +10,7 @@ b=12
 c=a+b
 t1="yarley"
 t2="mechanic"
+
 print(c)
 print("hello "+t1)
 #gives hello yarley
@@ -17,4 +18,10 @@ print("hello",t1)
 #gives hello yarley
 print("hello",t1,t2) #clubbing variables
 
+a=a+1;print(a) #incrementing and printing simaltaneously
+a=a+1;print(a);a=a+1;print(a);a=a+1;print(a);a=a+1;print(a)
+#2
+#3
+#4
+#5
       
