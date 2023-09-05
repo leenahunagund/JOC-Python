f2504ad190fa2ea4f5ba8b71f00d014a28981786
Leena Hunagund <109791804+leenahunagund@users.noreply.github.com>
2023-09-05 11:30:56 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,8 @@
+
+import matplotlib.pyplot as plt
+#plt.plot([1,2,3,4,5],[1,4,9,16,25],'ro') #dots
+#plt.plot([1,2,3,4,5],[1,4,9,16,25],'r--') #red dashes lines
+#plt.plot([1,2,3,4,5],[1,4,9,16,25],'bs') #blue squares
+plt.plot([1,2,3,4,5],[1,4,9,16,25],'g^') #green triangles
+plt.ylabel("some values")
+plt.xlabel("values")
