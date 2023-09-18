0a86f57c34bcf3410ae93c6508a325da23bbf1f8
Leena Hunagund <109791804+leenahunagund@users.noreply.github.com>
2023-09-18 10:21:04 +0530

    Add files via upload

---
+++ magicsquare.py
@@ -0,0 +1,42 @@
+def magicsq(n):
+    magics=[]
+    for i in range(n):
+        l=[]
+        for j in range(n):
+            l.append(0)
+        magics.append(l)
+    i=n//2
+    j=n-1
+    
+    num=n*n
+    count=1      
+    
+    while(count<=num):
+        if(i==-1 and j==n):
+            j=n-2
+            i=0
+        else :
+            if(j==n):
+                j=0
+            if(i<0):
+                i=n-1
+        if(magics[i][j]!=0):
+           j=j-2
+           i=i+1
+           continue
+        else:
+            magics[i][j]=count
+            count+=1
+        i=i-1
+        j=j+1
+        
+    for i in range(n):
+        #l=[]
+        for j in range(n):
+            #  l.append(0)
+            print(magics[i][j],end=" ")
+        print()
+    
+magicsq(3)
+
+    
