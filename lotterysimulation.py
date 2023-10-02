442615e3da8cabfddd674aacafa8b18c8b107988
Leena Hunagund <109791804+leenahunagund@users.noreply.github.com>
2023-10-02 12:36:30 +0530

    Update lotterysimulation.py

--- lotterysimulation.py
+++ lotterysimulation.py
@@ -23,3 +23,26 @@ for i in range(7):
         account=account-100
     print(account)
 print("lottery number: ",lottery)''''
+
+
+'''import random
+import matplotlib.pyplot as plt
+
+account=0
+x=[]
+y=[]
+
+lottery=random.randint(1,10)
+for i in range(7):
+    x.append(i+1)
+    bet=random.randint(1,10)
+    print("bet=", bet)
+    if bet==lottery:
+        account=account+900-100
+    else:
+        account=account-100
+    print(account)
+    y.append(account)
+print("lottery number: ",lottery)
+plt.plot(x,y)
+plt.show()'''
