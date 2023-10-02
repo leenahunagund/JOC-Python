d26dbff5f8c7d6fa584c48a5cc4b8d10cda9df52
Leena Hunagund <109791804+leenahunagund@users.noreply.github.com>
2023-10-02 12:32:34 +0530

    Update lotterysimulation.py

--- lotterysimulation.py
+++ lotterysimulation.py
@@ -9,3 +9,17 @@ while(1):
     else:
         account=account-100
     print(account)
+    
+'''import random
+
+account=0
+lottery=random.randint(1,10)
+for i in range(7):
+    bet=random.randint(1,10)
+    print("bet=", bet)
+    if bet==lottery:
+        account=account+900-100
+    else:
+        account=account-100
+    print(account)
+print("lottery number: ",lottery)''''
