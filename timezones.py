2bb142a7c872e8a082ffc9ef200d4ef6009d8bf7
Leena Hunagund <109791804+leenahunagund@users.noreply.github.com>
2023-10-23 11:25:28 +0530

    Add files via upload

---
+++ timezones.py
@@ -0,0 +1,9 @@
+from datetime import datetime as dt
+import pytz
+
+timezones=pytz.all_timezones
+
+for i in range(len(timezones)):
+    zone=timezones[i]
+    tz=pytz.timezone(zone)
+    print("current time at : ", zone ," is: ",dt.now(tz))
